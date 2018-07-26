948e995043e0b81502ef1e9f7b3b17599055c1cc
uecor <1065792154@qq.com>
2018-07-26 17:01:47 +0800

    add usertranform

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\Api\UserRequest;
+use App\Transformers\UserTransformer;
 
 class UsersController extends Controller
 {
@@ -13,12 +14,12 @@ class UsersController extends Controller
         $verifyData = \Cache::get($request->verification_key);
 
         if (!$verifyData) {
-            return $this->response->error('验证码已失效', 422);
+            return $this->response->error('图片验证码已失效', 422);
         }
 
         if (!hash_equals($verifyData['code'], $request->verification_code)) {
             // 返回401
-            return $this->response->errorUnauthorized('验证码错误');
+            return $this->response->errorUnauthorized('短信验证码错误');
         }
 
         $user = User::create([
@@ -30,6 +31,17 @@ class UsersController extends Controller
         // 清除验证码缓存
         \Cache::forget($request->verification_key);
 
-        return $this->response->created();
+        return $this->response->item($user, new UserTransformer())
+            ->setMeta([
+                'access_token' => \Auth::guard('api')->fromUser($user),
+                'token_type' => 'Bearer',
+                'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+            ])
+            ->setStatusCode(201);
+    }
+
+    public function me()
+    {
+        return $this->response->item($this->user(), new UserTransformer());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
 ], function($api) {
 
     $api->group([
@@ -36,4 +37,22 @@ $api->version('v1', [
             ->name('api.authorizations.destroy');
 
     });
+
+      $api->group([
+      'middleware' => 'api.throttle',
+      'limit' => config('api.rate_limits.access.limit'),
+      'expires' => config('api.rate_limits.access.expires'),
+      ], function ($api) {
+          // 游客可以访问的接口
+
+          // 需要 token 验证的接口
+          $api->group(['middleware' => 'api.auth'], function($api) {
+              // 当前登录用户信息
+              $api->get('user', 'UsersController@me')
+                  ->name('api.user.show');
+          // 当前登录用户权限
+          $api->get('user/permissions', 'PermissionsController@index')
+              ->name('api.user.permissions.index');
+          });
+      });
 });
