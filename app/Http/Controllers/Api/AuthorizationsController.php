903840bd1b076cbd14ebf0521dd9472dc7f486a9
uecor <1065792154@qq.com>
2018-08-17 09:18:39 +0800

    用户绑定小程序

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
 use App\Http\Requests\Api\AuthorizationRequest;
+use App\Http\Requests\Api\WeappAuthorizationRequest;
 
 class AuthorizationsController extends Controller
 {
@@ -100,5 +101,63 @@ class AuthorizationsController extends Controller
         ]);
     }
 
+
+    // 根据客户端提交的 code，获取小程序 openid；
+    // 如果未找到对应的用户，则抛出 403 错误，小程序可根据报错跳转到登陆页面；
+    // 如果小程序提交了 code 及用户名密码后，验证用户名密码后，为用户绑定 openid；
+    // 如果根据 openid 找到对用的用户，则只更新 session_key；
+
+    public function weappStore(WeappAuthorizationRequest $request)
+        {
+            $code = $request->code;
+
+            // 根据 code 获取微信 openid 和 session_key
+            $miniProgram = \EasyWeChat::miniProgram();
+            $data = $miniProgram->auth->session($code);
+
+            // 如果结果错误，说明 code 已过期或不正确，返回 401 错误
+            if (isset($data['errcode'])) {
+                return $this->response->errorUnauthorized('code 不正确');
+            }
+
+            // 找到 openid 对应的用户
+            $user = User::where('weapp_openid', $data['openid'])->first();
+
+            $attributes['weixin_session_key'] = $data['session_key'];
+
+            // 未找到对应用户则需要提交用户名密码进行用户绑定
+            if (!$user) {
+                // 如果未提交用户名密码，403 错误提示
+                if (!$request->username) {
+                    return $this->response->errorForbidden('用户不存在');
+                }
+
+                $username = $request->username;
+
+                // 用户名可以是邮箱或电话
+                filter_var($username, FILTER_VALIDATE_EMAIL) ?
+                    $credentials['email'] = $username :
+                    $credentials['phone'] = $username;
+
+                $credentials['password'] = $request->password;
+
+                // 验证用户名和密码是否正确
+                if (!Auth::guard('api')->once($credentials)) {
+                    return $this->response->errorUnauthorized('用户名或密码错误');
+                }
+
+                // 获取对应的用户
+                $user = Auth::guard('api')->getUser();
+                $attributes['weapp_openid'] = $data['openid'];
+            }
+
+            // 更新用户数据
+            $user->update($attributes);
+
+            // 为对应用户创建 JWT
+            $token = Auth::guard('api')->fromUser($user);
+
+            return $this->respondWithToken($token)->setStatusCode(201);
+        }
 
 }
