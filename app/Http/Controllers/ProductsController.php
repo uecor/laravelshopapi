63f3d4ea4a8a53726085925fecb8014f6cfccd8a
uecor <1065792154@qq.com>
2018-08-30 11:12:58 +0800

    增加小程序注册接口

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -65,7 +65,7 @@ class ProductsController extends Controller
             // boolval() 函数用于把值转为布尔值
             $favored = boolval($user->favoriteProducts()->find($product->id));
         }
-        
+
         $reviews = OrderItem::query()
             ->with(['order.user', 'productSku']) // 预先加载关联关系
             ->where('product_id', $product->id)
@@ -73,7 +73,7 @@ class ProductsController extends Controller
             ->orderBy('reviewed_at', 'desc') // 按评价时间倒序
             ->limit(10) // 取出 10 条
             ->get();
-        
+
         // 最后别忘了注入到模板中
         return view('products.show', [
             'product' => $product,
